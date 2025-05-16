86f13ba4045dbfcc99e15a0267251cfb09f2c6eb
Smccoy721 <146625293+Smccoy721@users.noreply.github.com>
2025-05-15 22:26:29 -0500

    changed case in file names

--- manuals.js
+++ manuals.js
@@ -2,6 +2,6 @@ const manuals = [
   {
     name: "M149A2",
     description: "400 Gallon Water Buffalo",
-    file: "manuals/m149a2-water-buffalo/TM-9-2330-267-13P.pdf"
+    file: "manuals/m149a2-water-buffalo/tm-9-2330-267-13p.pdf"
   }
 ];
