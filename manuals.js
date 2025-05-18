fc2f2502d10b58a5f21b96291ad3fff839ada613
Smccoy721 <146625293+Smccoy721@users.noreply.github.com>
2025-05-18 12:30:38 -0500

    fixed syntax error

--- manuals.js
+++ manuals.js
@@ -3,7 +3,7 @@ const manuals = [
     name: "M149A2",
     description: "400 Gallon Water Buffalo",
     file: "manuals/m149a2-water-buffalo/tm-9-2330-267-13p.pdf"
-  }
+  },
   {
     name: "H140 HEATER",
     description: "140,000 BTU SPACE HEATER",
