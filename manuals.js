a21ebe151678091a27773f776ecaeea3b0c19ba9
Smccoy721 <146625293+Smccoy721@users.noreply.github.com>
2025-05-18 12:28:22 -0500

    Added H140 Heater manual

--- manuals.js
+++ manuals.js
@@ -4,4 +4,11 @@ const manuals = [
     description: "400 Gallon Water Buffalo",
     file: "manuals/m149a2-water-buffalo/tm-9-2330-267-13p.pdf"
   }
+  {
+    name: "H140 HEATER",
+    description: "140,000 BTU SPACE HEATER",
+    file: "manuals/m149a2-water-buffalo/tm-9-4520-271-14.pdf"
+  }
+
+
 ];
